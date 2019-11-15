1cb173aa4605eeed4d93959c35ac0cd274b6d7db
dasNessie <mail@dasnessie.de>
2019-11-15 17:51:41 +0100

    move wishlist to / (from /wishlist)

--- dasnessie/dasnessie/urls.py
+++ dasnessie/dasnessie/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('wishlist/', include('wishlist.urls')),
+    path('', include('wishlist.urls')),
     path('admin/', admin.site.urls),
 ]
--- dasnessie/wishlist/views.py
+++ dasnessie/wishlist/views.py
@@ -1,5 +1,6 @@
 import random
 import string
+# import configparser
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
@@ -9,14 +10,27 @@ from wishlist.models import Wish
 
 def index(request):
     wish_list = Wish.objects.order_by('bought', '-importance')
+    # config = configparser.ConfigParser()
+    # config.read('wishlist/config')
+    # context_title = config['config']['titletext']
     context = {
         'wish_list': wish_list,
+        # 'titletext': context_title,
     }
     return render(request, 'wishlist/index.html', context)
 
+
 def bought(request, secret):
     wish = get_object_or_404(Wish, unbuy_string=secret)
-    return render(request, 'wishlist/bought.html', {'wish': wish})
+    # config = configparser.ConfigParser()
+    # config.read('wishlist/config')
+    # context_title = config['config']['titletext']
+    context = {
+        'wish': wish,
+        # 'titletext': context_title,
+    }
+    return render(request, 'wishlist/bought.html', context)
+
 
 def buy(request, wish_id):
     wish = get_object_or_404(Wish, pk=wish_id)
@@ -26,9 +40,10 @@ def buy(request, wish_id):
     wish.save()
     return HttpResponseRedirect(reverse('bought', args=(wish.unbuy_string,)))
 
+
 def unbuy(request, secret):
     wish = get_object_or_404(Wish, unbuy_string=secret)
     wish.bought = False
     wish.unbuy_string = None
     wish.save()
-    return HttpResponseRedirect('/wishlist')
+    return HttpResponseRedirect('/')
